f483bb15cc1172db45fc43d2051edd451691c2d1
Autorealm3 <autorealmny@gmail.com>
2026-06-14 14:37:45 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AutoRealm from "./AutoRealm.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AutoRealm />
-  </React.StrictMode>
-);
