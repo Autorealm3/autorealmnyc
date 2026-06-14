f9f6ce5fc60fbfbe281352c964868a5da9ce1e32
Autorealm3 <autorealmny@gmail.com>
2026-06-14 14:40:50 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AutoRealm from "./AutoRealm.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AutoRealm />
+  </React.StrictMode>
+);
